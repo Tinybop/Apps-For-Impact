124b7c66a6054953c4b09fbee7ad2ef6d0016c90
Josh Stewart <hi@joshstewart.me>
2015-07-27 15:13:05 -0400

    update build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,70 @@
-'use strict';
+(function(){
+  'use strict';
 
-// ============================================================================
-// Config
-// ============================================================================
+  // Config
+  // ==========================================================================
 
-var gulp = require('gulp'),
-  autoprefixer = require('gulp-autoprefixer'),
-  browserSync = require('browser-sync'),
-  del = require('del'),
-  minifyCss = require('gulp-minify-css'),
-  minifyHtml = require('gulp-minify-html'),
-  notify = require('gulp-notify'),
-  reload = browserSync.reload,
-  rev = require('gulp-rev'),
-  sass = require('gulp-sass'),
-  uglify = require('gulp-uglify'),
-  usemin = require('gulp-usemin');
+  var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    minifyCss = require('gulp-minify-css'),
+    minifyHtml = require('gulp-minify-html'),
+    notify = require('gulp-notify'),
+    rev = require('gulp-rev'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin');
 
 
-// ============================================================================
-// Tasks
-// ============================================================================
+  // Tasks
+  // ==========================================================================
 
-gulp.task('browser-sync', function () {
-  browserSync({
-    proxy: 'appsforimpact.dev',
-    notify: false,
-    logLevel: 'info',
-    logConnections: false,
-    logFileChanges: true
+  gulp.task('serve', ['styles'], function() {
+    browserSync({
+      proxy: 'appsforimpact.dev',
+      notify: false,
+      logLevel: 'info',
+      logConnections: true,
+      logFileChanges: true,
+      online: true,
+    });
+
+    gulp.watch('src/**/*.scss', ['styles']);
+    gulp.watch('dist/**/*').on('change', browserSync.reload);
   });
-});
 
-gulp.task('usemin', function () {
+  gulp.task('styles', function () {
 
-  del(['dist/css/*.css']);
+    del(['dist/css/*.css']);
 
-  return gulp.src('src/*.html')
-    .pipe(usemin({
-      css: [
-        sass({
-          errLogToConsole: false,
-          onError: function (err) {
-            return notify().write(err);
-          }
-        }),
-        autoprefixer('last 1 version', 'ie 9', 'ie 10', 'ios 6'),
-        minifyCss(),
-        'concat',
-        rev()
-      ],
-      html: [
-        minifyHtml({
-          empty: true
-        })
-      ],
-      js: [uglify(), rev()],
-      inlinejs: [uglify()],
-      inlinecss: [minifyCss(), 'concat']
-    }))
-    .pipe(gulp.dest('dist/'));
-});
+    return gulp.src('src/*.html')
+      .pipe(usemin({
+        css: [
+          sass({
+            errLogToConsole: false,
+            onError: function (err) {
+              return notify().write(err);
+            }
+          }),
+          autoprefixer('last 1 version', 'ie 9', 'ie 10', 'ios 6'),
+          minifyCss(),
+          'concat',
+          rev()
+        ],
+        html: [
+          minifyHtml({
+            empty: true
+          })
+        ],
+        js: [uglify(), rev()],
+        inlinejs: [uglify()],
+        inlinecss: [minifyCss(), 'concat']
+      }))
+      .pipe(gulp.dest('dist/'));
+  });
 
-gulp.task('watch', function () {
-  gulp.watch('src/**/*', ['usemin', reload]);
-});
 
-gulp.task('default', ['usemin', 'watch', 'browser-sync']);
+  gulp.task('default', ['serve']);
+
+})();
